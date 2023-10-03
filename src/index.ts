f30aac73f03ca1c05d4ed3475e0371fd44f5eac0
chiogen <iwashier@gmail.com>
2023-10-04 00:20:45 +0200

    Added new modules to index

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ export * from './array/first-item';
 export * from './array/last-item';
 export * from './array/chunk';
 export * from './array/compact';
+export * from './array/random-item';
 
 // Math
 export * from './math/clamp';
 export * from './math/round';
+export * from './math/random';
 
 // Type
 export * from './type/is-truly';
