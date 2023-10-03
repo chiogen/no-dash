f46a47281fa5ce2ecbbf0509f578c616b1bbe7e8
chiogen <iwashier@gmail.com>
2023-10-04 00:17:19 +0200

    Solved es6 import braces spaces

--- src/array/chunk.ts
+++ src/array/chunk.ts
@@ -1,4 +1,4 @@
-import {lastItem} from "./last-item";
+import { lastItem } from "./last-item";
 
 /** Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements. */
 export const chunk = <T>(array: T[], size = 1): T[][] => {
--- src/array/random-item.ts
+++ src/array/random-item.ts
@@ -1,4 +1,4 @@
-import {getRandomInt} from "../math/random";
+import { getRandomInt } from "../math/random";
 
 export const randomItem = <T>(list: ReadonlyArray<T>): T =>
     list[getRandomInt(list.length - 1)];
